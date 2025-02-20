d0387ae0014ba09de5b2c952575c282181d627aa
Jan Zisenis <jan.zisenis@gmail.com>
2025-02-20 07:38:41 +0100

    enabled flatpak for installation for other software

--- hosts/masterlaptop/config.nix
+++ hosts/masterlaptop/config.nix
@@ -452,7 +452,7 @@
     fstrim.enable = true;
     gvfs.enable = true;
     openssh.enable = true;
-    flatpak.enable = false;
+    flatpak.enable = true;
     printing = {
       enable = true;
       drivers = [
