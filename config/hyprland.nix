fadd10b07d4db2e9a34be81b7c677b7ac6d0a123
Jan Zisenis <jan.zisenis@gmail.com>
2025-03-31 07:09:15 +0200

    remove some not used stuff

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -2,14 +2,11 @@
   lib,
   username,
   host,
-  inputs,
-  pkgs,
   config,
   ...
 }:
 
 let
-  hyprplugins = inputs.hyprland-plugins.packages.${pkgs.system};
   inherit (import ../hosts/${host}/variables.nix)
     browser
     terminal
@@ -22,9 +19,6 @@ with lib;
     enable = true;
     xwayland.enable = true;
     systemd.enable = true;
-    # plugins = [
-    #   hyprplugins.hyprtrails
-    # ];
     extraConfig =
       let
         modifier = "SUPER";
@@ -43,7 +37,6 @@ with lib;
           env = QT_AUTO_SCREEN_SCALE_FACTOR, 1
           env = SDL_VIDEODRIVER, x11
           env = MOZ_ENABLE_WAYLAND, 1
-          ***REMOVED***
           exec-once = dbus-update-activation-environment --systemd --all
           exec-once = systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
           exec-once = killall -q swww;sleep .5 && swww init
